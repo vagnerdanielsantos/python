437c12aa2b40cfa935460226cbb211aa49b0d352
vagnerdanielsantos <Vagner.daniel.santoss@gmail.com>
2026-07-07 20:55:51 -0300

    Aula sobre tipos primitivos de dados

---
+++ Aulas/Aula_006.py
@@ -0,0 +1,12 @@
+numero01 = float (input(' Digite um número:  '))
+numero02 = float(input('Digite um número:  '))
+
+resultado_soma = numero01 + numero02
+resultado_subtracao = numero01 - numero02
+resultado_multiplicacao = numero01 * numero02
+resultado_divisao = numero01 / numero02
+
+print('A soma entre {} e {} vale: {} '.format(numero01, numero02, resultado_soma))
+print('A subtração entre {} e {} vale: {}: '.format(numero01, numero02, resultado_subtracao))
+print('A muultiplicação entre {} e {} vale: {}'.format(numero01, numero02, resultado_multiplicacao))
+print('A divisão entre {} e {} vale: {}'.format(numero01, numero02, resultado_divisao))
