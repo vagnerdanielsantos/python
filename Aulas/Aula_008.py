718c7003d169445a53534b7437d1588bdf957287
vagnerdanielsantos <Vagner.daniel.santoss@gmail.com>
2026-07-21 18:15:24 -0300

    Trabalhando com Modulos

---
+++ Aulas/Aula_008.py
@@ -0,0 +1,14 @@
+#### IMPORTA MÓDULOS
+
+#import math
+#numero = int(input("Digite um número inteiro: "))
+#raiz = math.sqrt(numero)
+#print("A raiz quadrada de {} é {:.0f}. ".format(numero , raiz))
+
+#from math import pow
+#numero = int(input("Digite um número inteiro: "))
+#potencia = pow(numero, 3)
+#print("A raíz cúbica de {} é {:.0f}. ".format(numero, potencia))
+
+import emoji
+print(emoji.emojize("Bem vindo !  💀", language='en'))
