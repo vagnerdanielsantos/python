e1957680ae7ddebfc382e59d54802530288126fb
vagnerdanielsantos <Vagner.daniel.santoss@gmail.com>
2026-08-05 20:38:19 -0300

    Tabuada V.1

---
+++ Exercicios/Exercicio009.py
@@ -0,0 +1,14 @@
+# FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E FAÇA A SUA TABUADA
+
+tabuada = int(input("Digite um número inteiro para mostrar a sua tabuada: "))
+
+print("{} x 01 = {}".format(tabuada, tabuada * 1))
+print("{} x 02 = {}". format(tabuada, tabuada * 2))
+print("{} x 03 = {}". format(tabuada, tabuada * 3))
+print("{} x 04 = {}". format(tabuada, tabuada * 4))
+print("{} x 05 = {}". format(tabuada, tabuada * 5))
+print("{} x 06 = {}". format(tabuada, tabuada * 6))
+print("{} x 07 = {}". format(tabuada, tabuada * 7))
+print("{} x 08 = {}". format(tabuada, tabuada * 8))
+print("{} x 09 = {}". format(tabuada, tabuada * 9))
+print("{} x 10 = {}". format(tabuada, tabuada * 10))
